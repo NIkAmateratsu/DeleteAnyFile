5e6b007813d2cb551631a71b118aa30b26074bda
NIkAmateratsu <niksbhatt07@gmail.com>
2018-11-19 12:22:40 +0530

    Add files via upload

---
+++ DeleteAnyFile.py
@@ -0,0 +1,20 @@
+import os
+
+filetodelete = input("Enter the file name:")
+os.chdir(r'd:/')
+path = os.getcwd()
+
+all_file = []
+print (all_file)
+for path, dirs, files in os.walk(path):
+      
+      for file in files:
+
+      	all_file.append(file)
+
+      	if file == filetodelete:
+
+      		os.chdir(path)
+      		os.unlink(file)
+      		print(file + ' is removed at ' + path)     		
+      		exit()
